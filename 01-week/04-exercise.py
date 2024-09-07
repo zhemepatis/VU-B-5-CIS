764c53e51e054ddce20098e7be086cb7eb77a0a6
zhemepatis <rink.gab@gmail.com>
2024-09-07 13:59:26 +0300

    added number rotation

---
+++ 01-week/04-exercise.py
@@ -0,0 +1,88 @@
+def coords_2_num(coords, side_len):
+    x, y = coords
+    return y * side_len + x
+
+
+def num_2_coords(num, side_len):
+    x = int(num % side_len)
+    y = int(num / side_len)
+    return (x, y)
+
+
+def get_square_level_num(side_len):
+    return int(side_len / 2 + side_len % 2)
+
+
+def get_point_layer(idx, side_len):
+    level_num = get_square_level_num(side_len)
+
+    if idx < level_num:
+        return idx % level_num
+    else:
+        return level_num - idx % level_num - 1 - side_len % 2
+
+
+def get_point_level(coords, side_len):
+    x, y = coords
+
+    x_layer = get_point_layer(x, side_len)
+    y_layer = get_point_layer(y, side_len)
+
+    return min(x_layer, y_layer)
+
+
+def rotate_num(num, side_len):
+    x, y = num_2_coords(num, side_len)
+    point_level = get_point_level((x, y), side_len)
+
+    alt_x = x - point_level
+    alt_y = y - point_level
+    alt_side_len = side_len - point_level * 2
+
+    if alt_y == 0:
+        alt_y += alt_x
+        alt_x = alt_side_len - 1
+    elif alt_x == alt_side_len - 1:
+        alt_x -= alt_y
+        alt_y = alt_side_len - 1
+    elif alt_y == alt_side_len - 1:
+        alt_y = alt_x
+        alt_x = 0
+    elif alt_x == 0:
+        alt_x = alt_side_len - 1 - alt_y
+        alt_y = 0
+
+    return coords_2_num((alt_x + point_level, alt_y + point_level), side_len)
+
+
+# reading data
+with open("04-data.txt") as data:
+    key = data.readline()
+    txt = data.read()
+
+# formatting data
+key = list(key.replace(" ", "").replace("\n", "").replace("[", "").replace("]", "").replace(",", ""))
+key = [int(x) for x in key]
+txt = txt.replace(" ", "")
+
+stencils = [key]
+prev_stencil = key
+side_len = len(key) + 1
+
+for i in range(0, 3):
+    rotated_stencil = []
+    for num in prev_stencil:
+        print(f"curr num: {num}")
+        rotated_num = rotate_num(num, side_len)
+        rotated_stencil.append(rotated_num)
+    
+    stencils.append(rotated_stencil)
+    prev_stencil = rotated_stencil
+
+print(stencils)
+
+
+
+
+
+
