fbd95a3c0bcdea49f0c08229d5f3268dc4ba4089
zhemepatis <rink.gab@gmail.com>
2024-12-04 13:19:31 +0200

    solved exercise 1 of 11th week

--- week-11/1.py
+++ week-11/1.py
@@ -2,8 +2,36 @@
 # Iššifruokite  šifrą 
 # 369412936784193964016448726382270848904560824373225. Tekstas keičiamas skaičiumi kaip RSA užduotyje.
 
+A = 'abcdefghijklmnopqrstuvwxyz '
+
+def i_teksta(M):
+    n = M
+    text = ''
+    while n > 0:
+        ind = n % 100
+        ind = ind - 1
+        if 0 <= ind < len(A):
+            text += A[ind]
+            n = (n - ind - 1) // 100
+        else:
+            text += '?'
+            n = (n - ind - 1) // 100
+    return text[::-1]
+
 p, q = 79496847203390844133441739, 79496847203390844133441987
 C = 369412936784193964016448726382270848904560824373225
 
-# sudarom viesa rakta
-n = p*q
+# calculating public key
+n = p * q
+
+# getting parts of message
+m1 = pow(C, (p + 1) // 4, p)
+m2 = pow(C, (q + 1) // 4, q)
+
+# calculating message using chineese reminders theorem
+p_inv = pow(p, -1, q)
+q_inv = pow(q, -1, p)
+m = (m1 * q * q_inv - m2 * p * p_inv) % n
+
+# printing message
+print(i_teksta(m))
